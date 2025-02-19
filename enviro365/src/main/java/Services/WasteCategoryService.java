270e0ce52c380da703ad99430ab872d99fb27a4e
katdev16 <katlegodhlamini2003@gmail.com>
2025-02-19 23:07:20 +0200

    added save,delete,update

--- enviro365/src/main/java/Services/WasteCategoryService.java
+++ enviro365/src/main/java/Services/WasteCategoryService.java
@@ -17,4 +17,24 @@ class WasteCategoryService {
     public List<WasteCategory> getAllCategories() {
         return repository.findAll();
     }
+    public WasteCategory getCategoryById(int id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    public WasteCategory saveCategory(WasteCategory category) {
+        return repository.save(category);
+    }
+
+    public WasteCategory updateCategory(int id, WasteCategory categoryDetails) {
+        WasteCategory category = getCategoryById(id);
+        category.setName(categoryDetails.getName());
+        category.setDescription(categoryDetails.getDescription());
+        category.setRecyclingTip(categoryDetails.getRecyclingTip());
+        return repository.save(category);
+    }
+
+    public void deleteCategory(int id) {
+        WasteCategory category = getCategoryById(id);
+        repository.delete(category);
+    }
 }
