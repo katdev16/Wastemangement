a20286ebe6690e53f2b6f781971641444c111644
katdev16 <katlegodhlamini2003@gmail.com>
2025-02-19 22:16:53 +0200

    spring added

---
+++ enviro365/src/main/java/com/enviro/enviro365/Enviro365Application.java
@@ -0,0 +1,13 @@
+package com.enviro.enviro365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365Application.class, args);
+	}
+
+}
---
+++ enviro365/src/test/java/com/enviro/enviro365/Enviro365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.enviro365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
